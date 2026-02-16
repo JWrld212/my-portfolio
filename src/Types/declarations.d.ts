127dc07c4830d5750ea4c9cf4cc47c5b7afeb68a
Ola2080 <abdulwajudolasunkanmi@gmail.com>
2026-02-16 21:48:10 +0100

    prepare for railway deploy

--- src/Types/declarations.d.ts
+++ src/Types/declarations.d.ts
@@ -2,3 +2,6 @@ declare module "*.css" {
   const content: { [className: string]: string };
   export default content;
 }
+
+// Provide a simple declaration for react-scroll to avoid build-time errors
+declare module "react-scroll";
